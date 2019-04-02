dab242821fbd06a8806f68e10519bb5c807e0ef6
sendysucc <sendysucc@gmail.com>
2019-04-02 18:34:04 +0800

    add model

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    GENDER = {
+        ('男', 1),
+        ('女',0),
+    }
+
+    userid = models.AutoField(primary_key=True)
+    username = models.CharField('用户名',max_length=15)
+    nickname = models.CharField('昵称',max_length=30)
+    avatoridx = models.IntegerField('头像id')
+    gender = models.IntegerField('性别', choices=GENDER)
+    cellphone = models.CharField('手机号',max_length=11)
+    password = models.CharField('密码',max_length=41)
+    gold = models.IntegerField('金币')
+    diamond = models.IntegerField('钻石')
+    createtime = models.DateTimeField(auto_now=True)
+    agentid = models.ForeignKey('Agent',db_column='agentid',blank=True,null=True,on_delete=models.SET_NULL)
+    promotecode = models.CharField('推广码'，max_length=10)
+    referrer = models.CharField('推荐人', max_length = 10)
+    accountdisable = models.BooleanField('账号是否被封',default=False)
+    isrobot = models.BooleanField('是否为机器人', default=False)
+
+    class Meta:
+        db_table = 'User'
+
+
+class Agent(models.Model):
+    agentid = models.AutoField(primary_key=True)
+    name = models.CharField('代理名',max_length=30)
+    acode = models.CharField('代理编码', max_length=20)
+    cellphone = models.CharField('手机号',max_length=11)
+    password = models.CharField('密码', max_length=40)
+    gold = models.IntegerField('金币')
+    diamond = models.IntegerField('钻石')
+    createtime = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'Agent'
+
+
--- z2owebend/settings.py
+++ z2owebend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'manager.app.ManagerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'z2owebend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.mysql',
+        'NAME' : 'z2oserver',
+        'USER' : 'sendy',
+        'PASSWORD':'sendy',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
