bcbb9ff7bd51e72215bb1304be8fa38432f5198d
sendysucc <sendysucc@gmail.com>
2019-04-04 11:49:03 +0800

    clean

--- manager/models.py
+++ manager/models.py
@@ -18,9 +18,9 @@ class User(models.Model):
     gold = models.IntegerField('金币')
     diamond = models.IntegerField('钻石')
     createtime = models.DateTimeField(auto_now=True)
-    agentid = models.ForeignKey('Agenter',db_column='agentid',blank=True,null=True,on_delete=models.SET_NULL)
-    promotecode = models.CharField('推广码',max_length=10)
-    referrer = models.CharField('推荐人', max_length = 10)
+    agentid = models.ForeignKey('Agenter',db_column='agentid',blank=True,null=True,on_delete=models.SET_NULL)   #对用户不可见，可根据app包或者url来标识
+    promotecode = models.CharField('推广码',max_length=10)      #用户自己的推广码，用于推广给其他新用户，来获取收益
+    referrer = models.CharField('推荐人', max_length = 10)      #注册时的推荐人， 就是其他用户的推广码
     accountdisable = models.BooleanField('账号是否被封',default=False)
     isrobot = models.BooleanField('是否为机器人', default=False)
 
@@ -52,6 +52,7 @@ class LoginRecord(models.Model):
 
 class Notice(models.Model):
     id = models.AutoField(primary_key=True)
+    header = models.CharFiled(max_length=30)
     msg = models.CharField(max_length=300)
     pubtime = models.DateTimeField()
 
@@ -60,6 +61,7 @@ class Notice(models.Model):
 
 class Mail(models.Model):
     id = models.AutoField(primary_key=True)
+    header = models.CharField(max_length=30)
     msg = models.TextField()
     pubtime = models.DateTimeField(auto_now=True)
 
